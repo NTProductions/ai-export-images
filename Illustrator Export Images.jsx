43dd612ffa210574098ea387662f3f390361e8e9
Nate Lovell <Natelovell99@gmail.com>
2020-10-15 02:12:45 -0600

    Uploading correct file

---
+++ Illustrator Export Images.jsx
@@ -0,0 +1,22 @@
+﻿// Illustrator Exporting/Saving Images
+var document = app.activeDocument;
+
+// Export Types
+document.exportFile(File("~/Documents/test.gif"), ExportType.GIF);
+document.exportFile(File("~/Documents/test.jpg"), ExportType.JPEG);
+document.exportFile(File("~/Documents/test.png"), ExportType.PNG24);
+document.exportFile(File("~/Documents/test.png"), ExportType.PNG8);
+document.exportFile(File("~/Documents/test.svg"), ExportType.SVG);
+document.exportFile(File("~/Documents/test.tiff"), ExportType.TIFF);
+
+// Save Types
+var saveOptions = new IllustratorSaveOptions();
+var aiDoc = new File("~/Documents/");
+document.saveAs(aiDoc, saveOptions);
+
+var saveOpts = new PDFSaveOptions();
+var saveName = new File("~/Documents/");
+saveOpts.preserveEditability = true;
+document.saveAs(saveName, saveOpts);
+
+alert("done");
